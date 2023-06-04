323d335c82561b40565be0cb9bd6bdd9bf57435b
Patryk Warchoł <patryk.t.warchol@gmail.com>
2023-06-04 19:45:23 +0200

    add api url to environment file

--- ui/src/app/service/comics.service.ts
+++ ui/src/app/service/comics.service.ts
@@ -2,12 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ComicResponse} from "../model/ComicResponse";
 import {UrlTitle} from "../model/UrlTitle";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComicsService {
-  private URL: string = 'http://localhost:8080/api/v1/comic/';
+  private URL: string = environment.apiUrl + 'comic/';
 
   constructor(private http: HttpClient) {
   }
